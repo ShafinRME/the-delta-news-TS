46e0489132e2d5825a6d18bf29e612ba29feca3a
mdHasif2001 <iamhasib727@gmail.com>
2022-08-26 02:58:50 +0600

    chat bot added

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Share/Header/Header";
 import Footer from "../components/Share/Footer/Footer";
+import { MessengerChat } from "react-messenger-chat-plugin";
+
 
 const MainLayout = () => {
   return (
@@ -11,6 +13,34 @@ const MainLayout = () => {
       </header>
       <main className="container mx-auto">
         <Outlet />
+        <MessengerChat
+          pageId="102918359220095"
+          language="sv_SE"
+          themeColor={"#F6621E"}
+          bottomSpacing={300}
+          loggedInGreeting="loggedInGreeting"
+          loggedOutGreeting="loggedOutGreeting"
+          greetingDialogDisplay={"show"}
+          debugMode={true}
+          onMessengerShow={() => {
+            console.log("onMessengerShow");
+          }}
+          onMessengerHide={() => {
+            console.log("onMessengerHide");
+          }}
+          onMessengerDialogShow={() => {
+            console.log("onMessengerDialogShow");
+          }}
+          onMessengerDialogHide={() => {
+            console.log("onMessengerDialogHide");
+          }}
+          onMessengerMounted={() => {
+            console.log("onMessengerMounted");
+          }}
+          onMessengerLoad={() => {
+            console.log("onMessengerLoad");
+          }}
+        />,
       </main>
 
       <footer className="container mx-auto">
