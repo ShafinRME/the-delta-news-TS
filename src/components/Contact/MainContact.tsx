8502bedbd8fc4c49045dea5786bbed953ff73741
mdHasif2001 <iamhasib727@gmail.com>
2022-08-26 15:14:36 +0600

    new contact page code push again

--- src/components/Contact/MainContact.tsx
+++ src/components/Contact/MainContact.tsx
@@ -6,10 +6,10 @@ const MainContact = () => {
     return (
         <div className='antialiased'>
             <div className='flex w-full justify-center items-center'>
-                    <div className='bg-error-content w-full max-w-4xl p-8 rounded-xl shadow-lg text-success flex flex-col space-y-6 md:flex-row md:space-x-6 md:space-y-0 sm:p-12 overflow-hidden'>
-                        <InputContact/>
-                        <DesignContact/>
-                    </div>
+                <div className='bg-success-content w-full max-w-4xl md:p-4 rounded-xl shadow-lg flex flex-col space-y-2 md:flex-row md:space-x-6 md:space-y-0 sm:p-12 overflow-hidden'>
+                    <DesignContact />
+                    <InputContact />
+                </div>
             </div>
         </div>
     );
