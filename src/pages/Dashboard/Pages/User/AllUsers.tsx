ac0251256ae524027c687de5a1f4cef33a965da1
ferdousr3 <ferdoour1992@gmail.com>
2022-08-31 10:25:38 +0600

    #76 fix more error

--- src/pages/Dashboard/Pages/User/AllUsers.tsx
+++ src/pages/Dashboard/Pages/User/AllUsers.tsx
@@ -31,55 +31,61 @@ const AllUsers = () => {
   if (isLoading) {
     return <Loadings />;
   }
-  if (isError) {
+  if (isError ) {
     return <h1>{error.message}</h1>;
   }
 
-  console.log("user", user);
+  console.log("user all user", user);
   return (
     <>
-      <section className="p-5 text-lg font-semibold text-left text-accent">
-        All Users
-        <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
-          Browse a list of Flowbite products designed to help you work and play,
-          stay organized, get answers, keep in touch, grow your business, and
-          more.
-        </p>
-      </section>
-      <section className="lg:max-w-6xl mx-auto overflow-x-auto">
-        <table className="table rounded-0 w-full">
-          {/* table title */}
-          <thead className="static top-20 w-full rounded-0">
-            <tr>
-              <th>SL</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Role</th>
-              <th>Make Admin</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          {/* table data */}
-          <tbody>
-            {data?.map((user, index) => (
-              <UserRow
-                key={user._id}
-                index={index}
+      {data?.length <= 0  ? (
+        <Loadings />
+      ) : (
+        <>
+          <section className="p-5 text-lg font-semibold text-left text-accent">
+            All Users
+            <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+              Browse a list of Flowbite products designed to help you work and
+              play, stay organized, get answers, keep in touch, grow your
+              business, and more.
+            </p>
+          </section>
+          <section className="lg:max-w-6xl mx-auto overflow-x-auto">
+            <table className="table rounded-0 w-full">
+              {/* table title */}
+              <thead className="static top-20 w-full rounded-0">
+                <tr>
+                  <th>SL</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Role</th>
+                  <th>Make Admin</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              {/* table data */}
+              <tbody>
+                {data?.map((user, index) => (
+                  <UserRow
+                    key={user._id}
+                    index={index}
+                    setUser={setUser}
+                    user={user}
+                  />
+                ))}
+              </tbody>
+            </table>
+
+            {user && (
+              <UserDeleteConfirmModal
                 setUser={setUser}
+                refetch={refetch}
                 user={user}
               />
-            ))}
-          </tbody>
-        </table>
-
-        {user && (
-          <UserDeleteConfirmModal
-            setUser={setUser}
-            refetch={refetch}
-            user={user}
-          />
-        )}
-      </section>
+            )}
+          </section>
+        </>
+      )}
     </>
   );
 };
